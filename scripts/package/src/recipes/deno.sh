72cd18dee6de8c86e774faae8b864011da220076
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-07-04 22:12:09 +0200

    Added deno recipe

---
+++ scripts/package/src/recipes/deno.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+deno::install() {
+  if platform::command_exists cargo; then
+    cargo install deno
+  else
+    "${SLOTH_PATH:-$DOTLY_PATH}/bin/dot" package add --skip-recipe deno
+  fi
+
+  if ! platform::command_exists deno &&
+     ! platform::command_exists curl
+  then
+    script::depends_on curl
+  fi
+
+  if platform::command_exists curl; then
+    curl -fsSL https://deno.land/x/install/install.sh | sh
+  fi
+
+  if platform::command_exists deno; then
+    return
+  fi
+
+  return 1
+}
+
+deno::is_installed() {
+  platform::command_exists deno
+}
