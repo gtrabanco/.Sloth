dffb05c388216211aafc900fb4e38d12ad1a129f
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-07-06 14:14:47 +0200

    Fixed arguments. Deleted option '-d' in linter when gives the argument '--patch'

--- scripts/core/src/dotly.sh
+++ scripts/core/src/dotly.sh
@@ -6,6 +6,6 @@ dotly::list_bash_files() {
 }
 
 dotly::list_dotfiles_bash_files() {
-  grep "#!/usr/bin/env bash" "$DOTFILES_PATH"/{bin,scripts,shell,restoration_scripts} -R | awk -F':' '{print $1}'
-  find "$DOTFILES_PATH"/{bin,scripts,shell,restoration_scripts} -type f -name "*.sh" -print0 | xargs -0 -I _ echo _
+  grep "#!/usr/bin/env bash" "${DOTFILES_PATH}/"{bin,scripts,shell,restoration_scripts} -R | awk -F':' '{print $1}'
+  find "${DOTFILES_PATH}/"{bin,scripts,shell,restoration_scripts} -type f -name "*.sh" -print0 | xargs -0 -I _ echo _
 }
