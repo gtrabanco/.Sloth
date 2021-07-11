82d7697c795140310f2bf7fafaeec6693a808d8a
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-07-11 16:51:00 +0200

    Fixed recipe to use python yq

--- scripts/package/src/recipes/python-yq.sh
+++ scripts/package/src/recipes/python-yq.sh
@@ -1,24 +1,33 @@
-dot::get_script_src_path "yq.sh" "package"
+#!/usr/bin/env bash
 
-yq::install() {
-  local binary_name yq_path
-
-  binary_name="yq_$(installer::get_os)_$(installer::get_arch)"
-  yq_path="$HOME/bin/yq"
-  output::error "yq not installed, installing"
-
-  "$DOTLY_PATH/bin/dot" package add python-yq --skip-recipe | log::file "Installing yq"
-  if package::is_installed python-yq; then
+python-yq::install() {
+  if
+    platform::command_exists brew &&
+    brew install python-yq &&
+    python-yq::is_installed
+  then
     return 0
   fi
 
-  if platform::command_exists pip3 &&
-    pip3 install yq | log::file "Installing yq from pip3" &&
-    platform::command_exists yq; then
+  if
+    platform::command_exists pip3 &&
+    pip3 install yq &&
+    python-yq::is_installed
+  then
     output::solution "yq installed!"
     return 0
   fi
 
   output::error "yq could not be installed"
   return 1
+}
+
+python-yq::is_installed() {
+  {
+    platform::command_exists brew && {
+      brew list --formula "python-yq" &> /dev/null || brew list --cask "python-yq"
+    }
+  } || {
+    platform::command_exists pip3 && pip3 show "yq" &> /dev/null
+  }
 }
