0f21b496f492a3b297f17e2ada441a3631baa0a5
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-07-14 18:53:34 +0200

    Added full functions of registry recipe

--- scripts/package/src/recipes/cargo.sh
+++ scripts/package/src/recipes/cargo.sh
@@ -24,3 +24,30 @@ cargo::install() {
 cargo::is_installed() {
   platform::command_exists cargo
 }
+
+cargo::version() {
+  local -r cargo="$(cargo --version | awk '{$2}')"
+  local -r rustup="$(rustup --version 2>/dev/null | head -n1 | awk '{print $2}')"
+  local -r rustc="$(rustc --version | awk '{print $2}')"
+  echo -n "${cargo} (rustup ${rustup} - rustc ${rustc})"
+}
+
+cargo::latest() {
+  rustup update --no-self-update 2>/dev/null | xargs | awk '{print $5}'
+}
+
+cargo::is_outdated() {
+  [[ "$(rustup update --no-self-update 2>/dev/null | xargs | awk '{print $2}')" == "updated" ]]
+}
+
+cargo::update() {
+  rustup update
+}
+
+cargo::description() {
+  echo -n "Cargo is the Rust package manager. Cargo downloads your Rust package's dependencies, compiles your packages, makes distributable packages, and uploads them to crates.io, the Rust community’s package registry."
+}
+
+cargo::url() {
+  echo -n "https://doc.rust-lang.org/cargo/ - https://crates.io"
+}
--- scripts/package/src/recipes/deno.sh
+++ scripts/package/src/recipes/deno.sh
@@ -26,3 +26,31 @@ deno::install() {
 deno::is_installed() {
   platform::command_exists deno
 }
+
+deno::is_outdated() {
+  ! deno upgrade --dry-run | tail -n 1 | grep -q "recent release$"
+}
+
+deno::upgrade() {
+  deno upgrade
+}
+
+deno::description() {
+  echo "Deno is a simple, modern and secure runtime for JavaScript and TypeScript that uses V8 and is built in Rust."
+}
+
+deno::url() {
+  echo "https://deno.land/"
+}
+
+deno::version() {
+  deno --version | head -n1 | awk '{print $2}'
+}
+
+deno::latest() {
+  if deno::is_outdated; then
+    deno upgrade --dry-run | head -n2 | tail -n1 | awk '{print $NF}'
+  else
+    deno::version
+  fi
+}
