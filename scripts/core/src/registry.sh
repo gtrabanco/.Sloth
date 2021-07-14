cd7e3903d80cbe7dc122b811108efb360be1bc37
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-07-15 01:59:44 +0200

    Final version of registry library

--- scripts/core/src/registry.sh
+++ scripts/core/src/registry.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# First added paths prevails over lasts
 export SLOTH_RECIPE_PATHS=(
   "${SLOTH_RECIPES_PATH[@]:-}"
   "${DOTFILES_PATH:-}/package/recipes"
@@ -55,7 +56,7 @@ registry::command_exists() {
   local -r recipe_file_path="$(registry::recipe_exists "$recipe")"
   [[ -z "${1:-}" || -z "${2:-}" || -z "${recipe_file_path}" ]] && return 1
 
-  script::function_exists "$recipe_command" "$recipe_file_path"
+  script::function_exists "$recipe_file_path" "$recipe_command"
 }
 
 #;
@@ -64,10 +65,18 @@ registry::command_exists() {
 # @param string recipe
 # @param string command
 # @param any optional args
-# @return boolean
+# @return any Whatever the command return
 #"
 registry::command() {
-  echo
+  local -r recipe="${1:-}"
+  local -r command="${2:-}"
+  local -r recipe_command="${recipe}::${command}"
+  [[ -z "$recipe" || -z "$command" ]] && return 1
+  shift 2
+
+  registry::command_exists "$recipe" "$command" &&
+    registry::load_recipe "$recipe" &&
+    "$recipe_command" "$@"
 }
 
 #;
@@ -78,19 +87,10 @@ registry::command() {
 #"
 registry::install() {
   local -r recipe="${1:-}"
-  local -r install_command="${recipe}::install"
-  local -r recipe_file_path="$(registry::recipe_exists "$recipe")"
+  local -r command="install"
+  [[ -z "$recipe" ]] && return 1
 
-  [[ -z "$recipe" || -z "$recipe_file_path" ]] && return 1
-
-  # TODO This is not finish
-  if registry; then
-    dot::load_library "$recipe_file_path"
-    "$install_command"
-    return $?
-  fi
-
-  return 1
+  registry::command "$recipe" "${command}"
 }
 
 #;
@@ -101,50 +101,153 @@ registry::install() {
 #"
 registry::is_installed() {
   local -r recipe="${1:-}"
-  local -r is_installed_command="${recipe}::is_installed"
-  [[ -z "$recipe" || -z "$(registry::recipe_exists "$recipe")" ]] && return 1
+  local -r command="is_installed"
+  [[ -z "$recipe" ]] && return 1
 
-  # TODO Change this
-  dot::load_library "$recipe_file_path"
-
-  if [[ "$(command -v "$is_installed_command")" ]]; then
-    "$is_installed_command"
-    return $?
-  fi
-
-  return 1
+  registry::command "$recipe" "${command}"
 }
 
 #;
-# registry::outdated()
+# registry::is_outdated()
 # Return if a recipe is outdated
 # @param string recipe
 # @return boolean
 #"
-registry::outdated() {
-  # TODO
-  echo
+registry::is_outdated() {
+  local -r recipe="${1:-}"
+  local -r command="is_outdated"
+  [[ -z "$recipe" ]] && return 1
+
+  registry::command "$recipe" "${command}"
 }
 
 #;
-# registry::update()
+# registry::upgrade()
 # Update a recipe. Will output the process
 # @param string recipe
 # @return boolean If error
 #"
-registry::update() {
-  # TODO
-  echo
+registry::upgrade() {
+  local recipe_title icon="📃"
+  local -r recipe="${1:-}"
+  
+  if [[ -z "$recipe" ]] || ! registry::is_installed "$recipe"; then
+    return 1
+  fi
+
+  recipe_title="$(registry::_recipe_title)"
+
+  output::empty_line
+  registry::_recipe_info "$recipe"
+  output::empty_line
+
+  if registry::command_exists "$recipe" "is_outdated"; then
+    if registry::is_outdated "$recipe"; then
+      registry::command "$recipe" "upgrade"  | log::file "Updating ${icon} recipe app: ${recipe_title}"
+    else
+      output::solution "${icon} Already has lastest version of ${recipe_title}"
+    fi
+    output::empty_line
+  elif registry::command_exists "$recipe" "upgrade"; then
+    output::answer "Can not check if ${recipe_title} is outdated, trying to update it."
+    registry::command "$recipe" "upgrade"  | log::file "Updating ${icon} recipe app: ${recipe_title}"
+    output::empty_line
+  fi
+}
+
+#;
+# registry::_recipe_title()
+# Private function to print the recipe title or default one
+# @param string recipe
+#"
+registry::_recipe_title() {
+  local icon="📃"
+  if registry::command_exists "$recipe" "title"; then
+    echo -n "$(registry::command "$recipe" "title")"
+  else
+    echo -n "${icon} ${recipe}"
+  fi
+}
+
+#;
+# registry::_recipe_info()
+# Private function to print the information about update a registry recipe
+# @param string recipe
+# @return void
+registry::_recipe_info() {
+  local version_message first_info last_pos last_info info recipe_all_info
+  local -r recipe="${1:-}"
+
+  recipe_all_info=("$(registry::_recipe_title)")
+
+  if registry::command_exists "$recipe" "version"; then
+    version_message="$(registry::command "$recipe" "version")"
+  fi
+
+  if registry::command_exists "$recipe" "latest"; then
+    version_message="${version_message} -> \`$(registry::command "$recipe" "latest")\`"
+    recipe_all_info+=("${version_message}")
+  elif [[ -n "${version_message:-}" ]]; then
+    version_message="Current: ${version_message}"
+    recipe_all_info+=("${version_message}")
+  fi
+
+  if registry::command_exists "$recipe" "description"; then
+    recipe_all_info+=("$(registry::command "$recipe" "description")")
+  fi
+
+  if registry::command_exists "$recipe" "url"; then
+    recipe_all_info+=("$(registry::command "$recipe" "url")")
+  fi
+
+  last_pos=$(( ${#recipe_all_info[@]} - 1 ))
+  last_info="${recipe_all_info[$last_pos]}"
+  first_info="${recipe_all_info[0]}"
+
+  if [[ ${#recipe_all_info[@]} -gt 0 ]]; then
+    for info in "${recipe_all_info[@]}"; do
+      if [[ $info == "$first_info" ]]; then
+        output::write "$info"
+      elif [[ $info != "$last_info" ]]; then
+        output::write " ├ ${info}"
+      else
+        output::write " └ ${info}"
+      fi
+    done
+  fi
 }
 
 #;
 # registry::list_all_recipes()
-# Get all available recipes. Gives user defined recipes preference over the core .Sloth recipes.
+# Get all available recipes with given command if provide the argument if not, gives all the available recipes. Note: Gives user defined recipes preference over the core .Sloth recipes.
+# @param string required_command If this optional param is set will give only the recipes with this command, you can provide multiple commands (without recipe:: at the beginning)
 # @return output each recipe full path per row (all of them)
 #"
 registry::list_all_recipes() {
-  # TODO
-  echo
+  local all_recipes_full_path recipe_path recipe_filename recipe recipes_name=() unique_recipes=()
+  readarray -t all_recipes_full_path < <(find "${SLOTH_RECIPE_PATHS[@]}" -maxdepth 1 -name "*.sh" -type f 2>/dev/null)
+  for recipe_path in "${all_recipes_full_path[@]}"; do
+    recipe_filename="$(basename "$recipe_path")"
+
+    # If exists due the paths order is a user defined recipe which prevails over the core recipe
+    array::exists_value "$recipe_filename" "${recipes_name[@]}" && continue
+
+    # Required functions
+    if [[ -n "${*:-}" ]]; then
+      recipe="${recipe_filename%.sh}"
+      has_all=true
+      for required_command in "$@"; do
+        ! script::function_exists "$recipe_path" "${recipe}::${required_command}" && has_all=false
+      done
+      ! $has_all && continue
+    fi
+
+    # All is going right
+    recipes_name+=("$recipe_filename")
+    unique_recipes+=("$recipe_path")
+  done
+
+  printf "%s\n" "${unique_recipes[@]}"
 }
 
 #;
@@ -152,30 +255,15 @@ registry::list_all_recipes() {
 # Update all available recipes that have defined, at least, the function ::update
 #"
 registry::update_all() {
-  # TODO
-  # only if recipe::update is present
-  # If have ::current_version, ::latest_version, ::description and ::url it will show that information before execute ::update
-  echo
-}
+  local recipe_file_path recipe_file_name recipe icon="📃"
 
-#;
-# registry::_check_many_functions_exists()
-# Private function to check if many functions exists on a recipe
-# @param string recipe
-# @params array functions
-# @return boolean Only true if exists all functions
-#"
-registry::_check_many_functions_exists() {
-  local fn_name fns_list
-  local -r recipe="${1:-}"
-  local -r recipe_file_path="$(registry::recipe_exists "$recipe")"
-  [[ -z "$recipe" || -z "$recipe_file_path" || ! -f "$recipe_file_path" ]] && return 1
-  shift
+  for recipe_file_path in $(registry::list_all_recipes "upgrade"); do
+    [[ -z "$recipe_file_path" || ! -f "$recipe_file_path" ]] && continue
+    recipe_file_name="$(basename "$recipe_file_path")"
+    recipe="${recipe_file_name%.sh}"
 
-  readarray -t fns_list < <(script::list_functions "$recipe_file_path")
-
-  for fn_name in "${@:-}"; do
-    [[ -z "$fn" ]] && continue
-    ! array::exists_value "${recipe}::${fn_name}" "${fns_list[@]}" && return 1
+    if registry::is_installed "$recipe"; then
+      registry::upgrade "$recipe"
+    fi
   done
 }
