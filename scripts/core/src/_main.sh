7c54e9f24b124932778057758184544297996a08
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-07-16 00:51:51 +0200

    Changed the order of core scripts are loaded

--- scripts/core/src/_main.sh
+++ scripts/core/src/_main.sh
@@ -3,7 +3,7 @@
 if ! ${DOT_MAIN_SOURCED:-false}; then
   # platform and output should be at the first place because they are used
   # in other libraries
-  for file in "${SLOTH_PATH:-$DOTLY_PATH}"/scripts/core/src/{platform,output,args,array,async,collections,documentation,dot,files,git,json,log,package,registry,script,str,yaml}.sh; do
+  for file in "${SLOTH_PATH:-$DOTLY_PATH}"/scripts/core/src/{platform,output,log,args,array,async,collections,documentation,dot,files,git,json,package,registry,script,str,yaml}.sh; do
     #shellcheck source=/dev/null
     . "$file" || exit 5
   done
