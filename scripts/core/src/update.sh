11816a76ab50e615f697d52635c862aac25ca05e
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-07-16 00:55:26 +0200

    Applied linter

--- scripts/core/src/update.sh
+++ scripts/core/src/update.sh
@@ -4,14 +4,11 @@ update::git_upstream_branch() {
   local upstream_branch
 
   upstream_branch="$(git::get_remote_head_upstream_branch "$@")"
-  if [[ -z "${upstream_branch}" ]]
-  then
-    git remote set-head origin --auto >/dev/null
+  if [[ -z "${upstream_branch}" ]]; then
+    git remote set-head origin --auto > /dev/null
     upstream_branch="$(git::get_remote_head_upstream_branch "$@")"
   fi
   upstream_branch="${upstream_branch#origin/}"
   [[ -z "${upstream_branch}" ]] && upstream_branch="master"
   echo "${upstream_branch}"
 }
-
-
