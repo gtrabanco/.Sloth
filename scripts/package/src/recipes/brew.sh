34318ebf7398e0cf4647fa8f90b956031a0178bc
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-07-18 17:31:24 +0200

    Added brew package manager as recipe

--- scripts/package/src/recipes/brew.sh
+++ scripts/package/src/recipes/brew.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#shellcheck disable=SC2034
 
 brew::execute_from_url() {
   [[ -z "${1:-}" ]] && return 1
@@ -31,32 +32,51 @@ brew::install() {
     if sudo -v; then
       apt install -y build-essential procps curl file git
     fi
-  elif platform::is_linux && platform::is_arm; then
+  elif [[ $(platform::get_arch) != "amd64" ]] && ! platform::is_macos_arm; then
     output::error "Brew is not supported"
     return 1
-  else
+  fi
+  brew::execute_from_url "$brew_install_script"
+
+  if [[ -d "/home/linuxbrew/.linuxbrew" && -x "/home/linuxbrew/.linuxbrew/bin/brew" ]]; then
+    BREW_BIN="/home/linuxbrew/.linuxbrew/bin/brew"
+  elif [[ -d "${HOME}/.linuxbrew" && -x "${HOME}/.linuxbrew/bin/brew" ]]; then
+    BREW_BIN="${HOME}/.linuxbrew/bin/brew"
+  elif [[ -x "/opt/homebrew/bin/brew" ]]; then
+    BREW_BIN="/opt/homebrew/bin/brew"
+  elif [[ -x "/usr/local/bin/brew" ]]; then
+    BREW_BIN="/usr/local/bin/brew"
+  elif command -v brew &> /dev/null; then
+    BREW_BIN="$(command -v brew)"
   fi
 
-if [[ -d "/home/linuxbrew/.linuxbrew" && -x "/home/linuxbrew/.linuxbrew/bin/brew" ]]; then
-  BREW_BIN="/home/linuxbrew/.linuxbrew/bin/brew"
-elif [[ -d "${HOME}/.linuxbrew" && -x "${HOME}/.linuxbrew/bin/brew" ]]; then
-  BREW_BIN="${HOME}/.linuxbrew/bin/brew"
-elif [[ -x "/usr/local/bin/brew" ]]; then
-  BREW_BIN="/usr/local/bin/brew"
-elif command -v brew &> /dev/null; then
-  BREW_BIN="$(command -v brew)"
-fi
+  HOMEBREW_PREFIX="$("$BREW_BIN" --prefix)"
+  HOMEBREW_CELLAR="${HOMEBREW_PREFIX}/Cellar"
+  HOMEBREW_REPOSITORY="${HOMEBREW_PREFIX}/Homebrew"
 
-  if platform::is_macos_arm; then
-    export PATH="$PATH:/opt/homebrew/bin:/usr/local/bin"
-  elif [[ -d ""]]
-  else
-    export PATH="$PATH:/usr/local/bin"
-  fi
-
+  PATH="${HOMEBREW_PREFIX}/bin:${HOMEBREW_PREFIX}/sbin:${PATH}"
+  export PATH HOMEBREW_PREFIX HOMEBREW_CELLAR HOMEBREW_REPOSITORY
 }
 
 brew::uninstall() {
   local -r brew_uninstall_script="https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh"
-  script::depends_on curl
+  brew::execute_from_url "$brew_uninstall_script"
 }
+
+brew::is_installed() {
+  if [[ -d "/home/linuxbrew/.linuxbrew" && -x "/home/linuxbrew/.linuxbrew/bin/brew" ]]; then
+    BREW_BIN="/home/linuxbrew/.linuxbrew/bin/brew"
+  elif [[ -d "${HOME}/.linuxbrew" && -x "${HOME}/.linuxbrew/bin/brew" ]]; then
+    BREW_BIN="${HOME}/.linuxbrew/bin/brew"
+  elif [[ -x "/opt/homebrew/bin/brew" ]]; then
+    BREW_BIN="/opt/homebrew/bin/brew"
+  elif [[ -x "/usr/local/bin/brew" ]]; then
+    BREW_BIN="/usr/local/bin/brew"
+  elif command -v brew &> /dev/null; then
+    BREW_BIN="$(command -v brew)"
+  fi
+
+  [[ -n "$BREW_BIN" ]]
+}
+
+# Brew update is done as package manager
