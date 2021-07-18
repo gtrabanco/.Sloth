01f4a4eaccd38a72e91d0a6a1fed6568700d80ec
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-07-18 19:47:41 +0200

    Added export/import for dnf

--- scripts/package/src/package_managers/dnf.sh
+++ scripts/package/src/package_managers/dnf.sh
@@ -30,3 +30,25 @@ dnf::is_installed() {
     [[ -n "${1:-}" ]] && platform::command_exists rpm && rpm -qa | grep -qw "${1:-}"
   fi
 }
+
+dnf::dump() {
+  DNF_DUMP_FILE_PATH="${1:-$DNF_DUMP_FILE_PATH}"
+
+  if package::common_dump_check apt "$DNF_DUMP_FILE_PATH"; then
+    dnf repoquery --qf '%{name}' --userinstalled \
+      | grep -v -- '-debuginfo$' \
+      | grep -v '^\(kernel-modules\|kernel\|kernel-core\|kernel-devel\)$' | tee "$DNF_DUMP_FILE_PATH" | log::file "Exporting ${dnf_title} packages"
+
+    return 0
+  fi
+
+  return 1
+}
+
+dnf::import() {
+  DNF_DUMP_FILE_PATH="${1:-$DNF_DUMP_FILE_PATH}"
+
+  if package::common_import_check apt "$DNF_DUMP_FILE_PATH"; then
+    xargs sudo dnf -y install < "$DNF_DUMP_FILE_PATH" | log::file "Importing ${dnf_title} packages"
+  fi
+}
