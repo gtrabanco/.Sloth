340e98fd40a82a7adaba07bc049a22279b12614d
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-07-29 23:29:58 +0200

    Emulates sdot functionality by not loading user env vars

---
+++ _raycast/sdot.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Same functionality as sdot command. dot without enviroment variables.
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon ⚫️
+# @raycast.argument1 { "type": "text", "placeholder": "Context", "optional": false }
+# @raycast.argument2 { "type": "text", "placeholder": "Script", "optional": false }
+# @raycast.argument3 { "type": "text", "placeholder": "Arguments for .Sloth script", "optional": true }
+# @raycast.packageName Productivity
+# @raycast.needsConfirmation false
+
+# Documentation:
+# @raycast.description Executes lazy .Sloth shell scripts
+# @raycast.author Gabriel Trabanco
+# @raycast.authorURL https://github.com/gtrabanco
+
+context="${1:-}"
+script="${2:-}"
+arg="${3:-}"
+
+if command -v dot &> /dev/null; then
+  dot "$context" "$script" $arg | sed 's/\x1b\[[0-9;]*[a-zA-Z]//g'
+else
+  echo "Error: dot is not installed for not login shell. Execute \`dot core install\` again"
+  exit 1
+fi
