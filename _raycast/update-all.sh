1268a37679c43e4b93fcc29913b0f3ea8d5c7ab5
Gabriel Trabanco <1969593+gtrabanco@users.noreply.github.com>
2021-07-29 02:28:09 +0200

    Added raycast scripts and instructions to set up (#114)
    Co-authored-by: Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>

---
+++ _raycast/update-all.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Update All System Packages
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon ♻️
+# @raycast.argument1 { "type": "text", "placeholder": "Package Manager", "optional": true }
+# @raycast.packageName System
+# @raycast.needsConfirmation false
+
+# Documentation:
+# @raycast.description Update all applications of all package managers that are supported by .Sloth
+# @raycast.author Gabriel Trabanco
+# @raycast.authorURL https://github.com/gtrabanco
+
+if command -v dot &> /dev/null; then
+  if [[ -n "${1:-}" && $1 != "all" ]]; then
+    dot package update_all "${1:-}" | sed 's/\x1b\[[0-9;]*[a-zA-Z]//g'
+  else
+    dot package update_all | sed 's/\x1b\[[0-9;]*[a-zA-Z]//g'
+  fi
+else
+  echo "Error: dot is not installed for not login shell. Execute \`dot core install\` again"
+  exit 1
+fi
