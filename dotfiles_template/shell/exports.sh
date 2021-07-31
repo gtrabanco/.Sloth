44de88d8d89cf8f16996f32ca8fc70eaf88e5333
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-08-01 01:07:54 +0200

    Added new vars configuration that were not added in v3

--- dotfiles_template/shell/exports.sh
+++ dotfiles_template/shell/exports.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+#shellcheck disable=SC2016
 # ------------------------------------------------------------------------------
 # GENERAL INFORMATION ABOUT THIS FILE
 # The variables here are loaded previously PATH is defined. Use full path if you
@@ -7,11 +9,24 @@
 # ------------------------------------------------------------------------------
 # Sloth config
 # ------------------------------------------------------------------------------
+export SLOTH_INIT_SCRIPTS=true       # Init scripts enabled (only defined false
 export SLOTH_AUTO_UPDATE_PERIOD_IN_DAYS=7
 export SLOTH_AUTO_UPDATE_MODE="auto" # silent, auto (default), info, prompt
-export SLOTH_UPDATE_VERSION="stable" # latest, stable (default), minor
-export SLOTH_INIT_SCRIPTS=true       # Init scripts enabled (only defined false
-# will deactivate this feature)
+export SLOTH_UPDATE_VERSION="stable" # latest, stable (default), or any specified
+# version if you want to pin to that version
+export SLOTH_ENV="production"                   # production or development. If
+# you define development all updates must be manually or when you have a clean
+# working directory and pushed all your commits.
+# This is done to avoid conflicts and lost changes.
+# For development all other configuration will be ignored and every time it
+# can be updated you will get the latest version.
+
+# These files should be added to the .gitignore and are used by .Sloth
+# to check for updates.
+# You can change the were these files are created and deleted
+export SLOTH_UPDATED_FILE='${DOTFILES_PATH}/.sloth_updated'
+export SLOTH_UPDATE_AVAILABE_FILE='${DOTFILES_PATH}/.sloth_update_available'
+export SLOTH_FORCE_CURRENT_VERSION_FILE='${DOTFILES_PATH}/.sloth_force_current_version'
 
 # ------------------------------------------------------------------------------
 # Codely theme config
