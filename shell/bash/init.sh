2904ecfa99c391f8ee4502f1ac4842e49cae2b37
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-07-31 23:37:03 +0200

    Applied linter

--- shell/bash/init.sh
+++ shell/bash/init.sh
@@ -9,7 +9,7 @@ __right_prompt() {
   RIGHT_PROMPT=""
   [[ -n $RPS1 ]] && RIGHT_PROMPT=$RPS1 || RIGHT_PROMPT=$RPROMPT
   if [[ -n $RIGHT_PROMPT ]]; then
-    n=$((COLUMNS - ${#RIGHT_PROMPT} + (${#RIGHT_PROMPT}/3) ))
+    n=$((COLUMNS - ${#RIGHT_PROMPT} + (${#RIGHT_PROMPT} / 3)))
     printf "%${n}s$RIGHT_PROMPT\\r"
   fi
 
