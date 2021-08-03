d1d13146f2b0cc05b5d666c48bdafe1c230fbcb6
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-08-03 15:43:56 +0200

    Linter and minor fixes

--- scripts/package/src/recipes/nvm.sh
+++ scripts/package/src/recipes/nvm.sh
@@ -5,20 +5,22 @@
 . "${SLOTH_PATH:-${DOTLY_PATH:-}}/shell/init.scripts/nvm"
 
 nvm::install_script() {
-  PROFILE="/dev/null" curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/$(nvm::latest)/install.sh" | bash
+  PROFILE="/dev/null" curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/v$(nvm::latest)/install.sh" | bash
 }
 
 nvm::finish_install() {
   nvm install --lts --latest-npm
+  sleep 1s
   nvm use --lts
+  sleep 1s
   nvm alias default node
 }
 
 nvm::is_installed() {
   if
     [[ -z "${NVM_DIR:-}" && -z "${XDG_CONFIG_HOME:-}" ]] &&
-    command -v brew &> /dev/null &&
-    [[ -n "$(brew --prefix nvm)" ]]
+      command -v brew &> /dev/null &&
+      [[ -n "$(brew --prefix nvm)" ]]
   then
     NVM_DIR="$(brew --prefix nvm)"
   elif [[ -z "${NVM_DIR:-}" && -z "${XDG_CONFIG_HOME:-}" ]]; then
@@ -58,7 +60,6 @@ nvm::install() {
     output::empty_line
 
     #shellcheck disable=SC1091
-    . "${SLOTH_PATH:-${DOTLY_PATH:-}}/shell/init.scripts/nvm" && output::solution "You can use nvm, node, npm and npx now"
     output::answer "Nvm, node, npm and npx installed"
     return 0
   fi
@@ -69,15 +70,18 @@ nvm::install() {
 nvm::uninstall() {
   [[ -d "${NVM_DIR:-}" ]] && rm -rf "${NVM_DIR:-}"
   [[ -e "${DOTFILES_PATH:-}/shell/init.scripts-enabled/nvm" ]] && rm -rf "${DOTFILES_PATH:-}/shell/init.scripts-enabled/nvm"
-  output::answer "nvm uninstalled"
+  ! nvm::is_installed && output::answer "nvm uninstalled" && return 0
+
+  output::error "NVM could not be uninstalled"
+  return 1
 }
 
 nvm::is_outdated() {
   ! platform::command_exists nvm && return 1
   local -r installed="$(nvm --version)"
-  local -r latest ="$(nvm::latest)"
+  local -r latest="$(nvm::latest)"
   [[ -z "$installed" || -z "$latest" ]] && return 1
-  [[ $(platform::semver_compare "$installed" "${latest//v/}") -eq -1 ]]
+  [[ $(platform::semver_compare "$installed" "$latest") -eq -1 ]]
 }
 
 nvm::upgrade() {
@@ -98,14 +102,14 @@ nvm::version() {
 }
 
 nvm::latest() {
+  # TODO when github.sh library will be implemented update this to use it
   curl --silent "https://api.github.com/repos/nvm-sh/nvm/releases/latest" |
     grep '"tag_name":' |
-    sed -E 's/.*"([^"]+)".*/\1/' || echo -n
+    sed -E 's/.*"v([^"]+)".*/\1/' || echo -n "0.38.0" # Fallback to latest known version
+  # Fallback is added because is the latest version in the date this recipe was created
+  # this allow to know at least one known latest when github api is not available
 }
 
 nvm::title() {
   echo -n "🟩 NVM"
 }
-
-
-
