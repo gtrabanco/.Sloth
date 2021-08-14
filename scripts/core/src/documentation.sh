542dca87396f93eeedbb0e407c6fee8343dfcf56
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-08-14 15:18:15 +0200

    Parse docopt with awk. Added function to parse a docopt section

--- scripts/core/src/documentation.sh
+++ scripts/core/src/documentation.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#shellcheck disable=SC2016
 
 docs::parse() {
   docs::parse_script "$0" "$@"
@@ -18,12 +19,39 @@ docs::parse_script() {
     exit 1
   fi
 
-  eval "$(docpars -h "$(grep "^##?" "${script_path}" | cut -c 5-)" : "$@")"
+  eval "$(docpars -h "$(command -p awk '/^##\?/ {sub(/^##\? ?/,"", $0); print $0}' < "$script_path")" : "$@")"
 }
 
 docs::parse_script_version() {
+  local version
   local -r SCRIPT_FULL_PATH="${1:-}"
 
-  [[ ! -f "$SCRIPT_FULL_PATH" ]] && return 1
-  awk '/SCRIPT_VERSION[=| ]"?(.[^";]*)"?;?$/ {gsub(/[=|"]/, " "); print $NF}' "$SCRIPT_FULL_PATH" | sort -Vr | head -n1
+  [[ ! -r "$SCRIPT_FULL_PATH" ]] && return 1
+  version="$(command -p awk '/SCRIPT_VERSION[=| ]"?(.[^";]*)"?;?$/ {gsub(/[=|"]/, " "); print $NF}' "$SCRIPT_FULL_PATH" | command -p sort -Vr | command -p head -n1)"
+
+  if [[ -z "$version" ]]; then
+    #command -p grep "^#?" "$SCRIPT_FULL_PATH" | command -p cut -c 4-
+    command -p awk '/^#\?/ {sub(/^#\? ?/,"", $0); print $0}' "$SCRIPT_FULL_PATH"
+  else
+    builtin echo "$version"
+  fi
+}
+
+docs::parse_docopt_section() {
+  if [[ -t 0 ]]; then
+    local -r SCRIPT_FULL_PATH="${1:-}"
+    local -r SECTION_NAME="${2:-}"
+    [[ -n "${SCRIPT_FULL_PATH:-}" && ! -r "${SCRIPT_FULL_PATH:-}" ]] && return 1
+  else
+    local -r SECTION_NAME="${1:-Usage}"
+    local -r SCRIPT_FULL_PATH="/dev/stdin"
+  fi
+
+  if [[ $SECTION_NAME == "Version" ]]; then
+    docs::parse_script_version "$SCRIPT_FULL_PATH"
+  else
+    #grep "^##?" "$SCRIPT_FULL_PATH" | cut -c 4- | command -p sed -n "/${section}:$/,/^$/ p" | command -p sed -e '1d' -e '$d'
+    #command -p awk '/^##\?/ {sub(/^##\? ?/,"", $0); print $0}' "$SCRIPT_FULL_PATH" | command -p sed -n "/^${section}:$/,/^$/ p" | command -p sed -e '1d' -e '$d'
+    command -p awk '/^##\?/ {sub(/^##\? ?/,"", $0); print $0}' <"$SCRIPT_FULL_PATH" | command -p sed -n "/${section}:/I,/^$/ p" | command -p sed -e '1d' -e '$d'
+  fi
 }
