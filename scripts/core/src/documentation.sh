b07118f4829dd8bb4e0f79560f3eb0cc8a8798d0
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-08-14 15:42:23 +0200

    Colored help, default --help and --version without docpars

--- scripts/core/src/documentation.sh
+++ scripts/core/src/documentation.sh
@@ -1,6 +1,37 @@
 #!/usr/bin/env bash
 #shellcheck disable=SC2016
 
+green='\033[0;32m'
+normal='\033[0m'
+
+docs::parse_docopt() {
+  local doc
+  doc="$(command -p awk '/^##\?/ {sub(/^##\? ?/,"", $0); print $0}' < "${1:-/dev/stdin}")"
+  doc="${doc//\\\$/\$}"
+  doc="${doc//\\\`/\`}"
+
+  doc="$(echo "$doc" |
+    command -p awk "{ORS=(NR+1)%2==0?\"${green}\":RS}1" RS="\`" |
+    command -p awk "{ORS=NR%1==0?\"${normal}\":RS}1" RS="\`")"
+
+  echo -e "$doc"
+}
+
+docs::parse_script_version() {
+  local version
+  local -r SCRIPT_FULL_PATH="${1:-}"
+
+  [[ ! -r "$SCRIPT_FULL_PATH" ]] && return 1
+  version="$(command -p awk '/SCRIPT_VERSION[=| ]"?(.[^";]*)"?;?$/ {gsub(/[=|"]/, " "); print $NF}' "$SCRIPT_FULL_PATH" | command -p sort -Vr | command -p head -n1)"
+
+  if [[ -z "$version" ]]; then
+    #command -p grep "^#?" "$SCRIPT_FULL_PATH" | command -p cut -c 4-
+    command -p awk '/^#\?/ {sub(/^#\? ?/,"", $0); print $0}' "$SCRIPT_FULL_PATH"
+  else
+    builtin echo "$version"
+  fi
+}
+
 docs::parse() {
   docs::parse_script "$0" "$@"
 }
@@ -17,24 +48,16 @@ docs::parse_script() {
   elif [[ ! -f "${script_path}" ]]; then
     output::error "The given script does not exists"
     exit 1
+
+  elif [[ $1 == "--help" || $1 == "-h" ]]; then
+    docs::parse_docopt "$script_path"
+    exit 0
+  elif [[ $1 == "--version" || $1 == "-v" ]]; then
+    docs::parse_script_version "$script_path"
+    exit 0
   fi
 
   eval "$(docpars -h "$(command -p awk '/^##\?/ {sub(/^##\? ?/,"", $0); print $0}' < "$script_path")" : "$@")"
-}
-
-docs::parse_script_version() {
-  local version
-  local -r SCRIPT_FULL_PATH="${1:-}"
-
-  [[ ! -r "$SCRIPT_FULL_PATH" ]] && return 1
-  version="$(command -p awk '/SCRIPT_VERSION[=| ]"?(.[^";]*)"?;?$/ {gsub(/[=|"]/, " "); print $NF}' "$SCRIPT_FULL_PATH" | command -p sort -Vr | command -p head -n1)"
-
-  if [[ -z "$version" ]]; then
-    #command -p grep "^#?" "$SCRIPT_FULL_PATH" | command -p cut -c 4-
-    command -p awk '/^#\?/ {sub(/^#\? ?/,"", $0); print $0}' "$SCRIPT_FULL_PATH"
-  else
-    builtin echo "$version"
-  fi
 }
 
 docs::parse_docopt_section() {
