6d35bb164b6df92d74b9b816b67e44aab3c30ac1
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-11-25 14:03:54 +0100

    Ignore templates

--- scripts/core/src/dotly.sh
+++ scripts/core/src/dotly.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 dotly::list_bash_files() {
-  grep "#!/usr/bin/env bash" "${SLOTH_PATH:-${DOTLY_PATH:-}}"/{bin,dotfiles_template,scripts,shell,installer,restorer} -R | awk -F':' '{print $1}'
-  find "${SLOTH_PATH:-${DOTLY_PATH:-}}"/{bin,dotfiles_template,scripts,shell} -type f -name "*.sh" -print0 | xargs -0 -I _ echo _
+  grep --exclude-dir=*template* "#!/usr/bin/env bash" "${SLOTH_PATH:-${DOTLY_PATH:-}}"/{bin,dotfiles_template,scripts,shell,installer,restorer} -R | awk -F':' '{print $1}' 2>/dev/null
+  find "${SLOTH_PATH:-${DOTLY_PATH:-}}"/{bin,dotfiles_template,scripts,shell} -type f -name "*.sh" -not -path "*template*" -print0 2>/dev/null | xargs -0 -I _ echo _
 }
 
 dotly::list_dotfiles_bash_files() {
-  grep "#!/usr/bin/env bash" "${DOTFILES_PATH}/"{bin,scripts,shell,restoration_scripts} -R | awk -F':' '{print $1}'
-  find "${DOTFILES_PATH}/"{bin,scripts,shell,restoration_scripts} -type f -name "*.sh" -print0 | xargs -0 -I _ echo _
+  grep --exclude-dir=*template* "#!/usr/bin/env bash" "${DOTFILES_PATH}/"{bin,scripts,shell,restoration_scripts} -R | awk -F':' '{print $1}' 2>/dev/null
+  find "${DOTFILES_PATH}/"{bin,scripts,shell,restoration_scripts} -type f -name "*.sh" "*/template" -not -path "*template*" -print0 2>/dev/null | xargs -0 -I _ echo _
 }
