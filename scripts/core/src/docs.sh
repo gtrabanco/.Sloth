1de78c648edd286eca671c8eca17291968b02095
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-12-16 16:06:45 +0100

    Refactored docs::parse_script in docs::parse

--- scripts/core/src/docs.sh
+++ scripts/core/src/docs.sh
@@ -4,20 +4,7 @@
 green='\033[0;32m'
 normal='\033[0m'
 
-docs::parse_docopt() {
-  local doc
-  doc="$(command -p awk '/^##\?/ {sub(/^##\? ?/,"", $0); print $0}' < "${1:-/dev/stdin}")"
-  doc="${doc//\\\$/\$}"
-  doc="${doc//\\\`/\`}"
-
-  doc="$(echo "$doc" |
-    command -p awk "{ORS=(NR+1)%2==0?\"${green}\":RS}1" RS="\`" |
-    command -p awk "{ORS=NR%1==0?\"${normal}\":RS}1" RS="\`")"
-
-  echo -e "${doc//\$0/${SCRIPT_NAME:-${BASH_SOURCE[0]:-}}}"
-}
-
-docs::parse_script_version() {
+docs::parse_version() {
   local version
   local -r SCRIPT_FULL_PATH="${1:-}"
 
@@ -38,34 +25,21 @@ docs::parse_script_version() {
   builtin echo "${version:-v0.0.0}"
 }
 
-docs::parse() {
-  docs::parse_script "$0" "$@"
+docs::parse_full_docopt() {
+  command -p awk '/^##\?/ {sub(/^##\? ?/,"", $0); print $0}' < "${1:-/dev/stdin}"
 }
 
-docs::parse_script() {
-  local -r script_path="${1:-}"
-  shift
-  if ! platform::command_exists docopts; then
-    output::error "You need to have docopts installed to use \`.Sloth\`"
-    output::solution "Run this command to install it:"
-    output::solution "DOTLY_INSTALLER=true dot package add docopts"
+docs::parse_docopt() {
+  local doc
+  doc="$(docs::parse_full_docopt < "${1:-/dev/stdin}")"
+  doc="${doc//\\\$/\$}"
+  doc="${doc//\\\`/\`}"
 
-    exit 1
-  elif [[ ! -f "${script_path}" ]]; then
-    output::error "The given script does not exists"
-    exit 1
+  doc="$(echo "$doc" |
+    command -p awk "{ORS=(NR+1)%2==0?\"${green}\":RS}1" RS="\`" |
+    command -p awk "{ORS=NR%1==0?\"${normal}\":RS}1" RS="\`")"
 
-  elif [[ "${1:-}" == "--help" || "${1:-}" == "-h" ]]; then
-    docs::parse_docopt "$script_path"
-    exit 0
-  elif [[ "${1:-}" == "--version" || "${1:-}" == "-v" ]]; then
-    docs::parse_script_version "$script_path"
-    exit 0
-  fi
-
-  script::depends_on docopts
-
-  eval "$(docopts -h "$(command -p awk '/^##\?/ {sub(/^##\? ?/,"", $0); print $0}' < "$script_path")" --version "$(docs::parse_script_version)" : "$@")"
+  echo -e "${doc//\$0/${SCRIPT_NAME:-${BASH_SOURCE[0]:-}}}"
 }
 
 docs::parse_docopt_section() {
@@ -74,9 +48,9 @@ docs::parse_docopt_section() {
   [[ -n "${SCRIPT_FULL_PATH:-}" && ! -r "${SCRIPT_FULL_PATH:-}" ]] && return 1
 
   if [[ $SECTION_NAME == "Version" ]]; then
-    docs::parse_script_version "$SCRIPT_FULL_PATH"
+    docs::parse_version "$SCRIPT_FULL_PATH"
   else
-    command -p awk '/^##\?/ {sub(/^##\? ?/,"", $0); print $0}' < "$SCRIPT_FULL_PATH" | command -p sed -n "/${SECTION_NAME}:/,/^$/ p" | command -p sed -e '1d' -e '$d'
+    docs::parse_full_docopt "$SCRIPT_FULL_PATH" | command -p sed -n "/${SECTION_NAME}:/,/^$/ p" | command -p sed -e '1d' -e '$d'
   fi
 }
 
@@ -101,4 +75,34 @@ docs::parse_docopt_argument() {
   show_args="${script_args[*]:-}"
 
   echo "$show_args"
+}
+
+# TODO
+docs::parse() {
+  local script_path
+  script_path="${1:-}"
+  local -r argc="${#BASH_SOURCE[*]}"
+  [[ -f "$script_path" ]] && shift || script_path="${BASH_SOURCE[$((argc - 1))]:-}"
+
+  if ! platform::command_exists docopts; then
+    output::error "You need to have docopts installed to use \`.Sloth\`"
+    output::solution "Run this command to install it:"
+    output::solution "DOTLY_INSTALLER=true dot package add docopts"
+
+    exit 1
+  elif [[ ! -f "${script_path}" ]]; then
+    output::error "The given script does not exists"
+    exit 1
+
+  elif [[ "${1:-}" == "--help" || "${1:-}" == "-h" ]]; then
+    docs::parse_docopt "$script_path"
+    exit 0
+  elif [[ "${1:-}" == "--version" || "${1:-}" == "-v" ]]; then
+    docs::parse_version "$script_path"
+    exit 0
+  fi
+
+  script::depends_on docopts
+
+  eval "$(docopts -h "$(docs::parse_full_docopt "$script_path")" : "$@")"
 }
