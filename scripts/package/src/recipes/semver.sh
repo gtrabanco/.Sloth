4dd4f4946e739d1add52dff46a418d146a0ab247
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-12-16 16:40:56 +0100

    Revert changes

--- scripts/package/src/recipes/semver.sh
+++ scripts/package/src/recipes/semver.sh
@@ -27,9 +27,10 @@ semver::install() {
     # output::answer "\`--force\` option is ignored with this recipe"
     semver::force_install "$@" && return
   else
-    semver::is_installed && return
+    semver::is_installed && output::solution "semver installed" && return
   fi
 
+  output::error "semver could not be installed"
   return 1
 }
 
